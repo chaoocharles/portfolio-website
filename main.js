0833af7f463f6269bbfd495ce04bc2a52db6a609
Chaoo Charles <charleykairu@gmail.com>
2020-07-06 19:26:02 +0300

    landing page and side-nav bar

---
+++ main.js
@@ -0,0 +1,18 @@
+// Function expression to select elements `
+
+const selectElement = (s) => document.querySelector(s);
+const navLinks = document.querySelectorAll(".nav-link");
+
+selectElement(".burger-menu-icon").addEventListener("click", () => {
+    selectElement(".nav-list").classList.toggle("active");
+    selectElement(".burger-menu-icon").classList.toggle("toggle")
+
+    navLinks.forEach((link, index) => {
+        if (link.style.animation){
+            link.style.animation = ""
+        }else{
+            link.style.animation = `navLinkAnimate 0.5s ease forwards ${ index/7 + 0.5}s`
+            console.log(index/7 + 0.5)
+        }
+    })
+});
