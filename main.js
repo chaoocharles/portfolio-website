d0348fe9c73c4b8fd5d1a9ee8efb184860656dbf
Chaoo Charles <charleykairu@gmail.com>
2020-07-20 14:29:24 +0300

    Portfolio complete

--- main.js
+++ main.js
@@ -16,3 +16,19 @@ selectElement(".burger-menu-icon").addEventListener("click", () => {
         }
     })
 });
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        selectElement(".nav-list").classList.toggle("active");
+        selectElement(".burger-menu-icon").classList.toggle("toggle");
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation){
+                link.style.animation = ""
+            }else{
+                link.style.animation = `navLinkAnimate 0.5s ease forwards ${ index/7 + 0.5}s`
+                console.log(index/7 + 0.5)
+            }
+        })
+    })
+})
